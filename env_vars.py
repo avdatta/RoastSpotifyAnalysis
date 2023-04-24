de5bcbb5b3b2e5c0df716a49e24b14ed95bbd21a
KingZuko <rs6121@nyu.edu>
2023-04-24 16:20:58 -0400

    we have GOTTHEPLAYLIST

--- env_vars.py
+++ env_vars.py
@@ -6,7 +6,7 @@ from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 # Set your Spotify API credentials
 client_id = "3c70b159bb9848ca890755bf3083e080"
 client_secret = "578f8808a62d4b92b5d8387d61c33432"
-redirect_uri = "https://curiouscaliboi.github.io"
+redirect_uri = "http://localhost:8888/callback/"
 
 # Set the required scope(s)
 scope = "user-library-read"
@@ -22,4 +22,10 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, auth
 
 
 
-# Now lets
+# Now lets work with our playlist
+playlist_id = "1pDpruzoY7JvPBq2CJujFs"
+# Got it baby whohhh
+playlist = sp.playlist(playlist_id)
+
+print(f"Playlist name: {playlist['name']}")
+print(f"Total tracks: {playlist['tracks']['total']}")
